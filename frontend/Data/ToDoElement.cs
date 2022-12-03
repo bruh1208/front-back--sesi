cc210752549dd42141faabbe074842907ce177cd
bruh1208 <118627359+bruh1208@users.noreply.github.com>
2022-12-03 16:41:22 -0300

    front quase terminado

---
+++ frontend/Data/ToDoElement.cs
@@ -0,0 +1,8 @@
+public class ToDoElement{
+
+    public string Descricao { get; set;}
+
+    public DateTime  Prazo {get; set;}
+
+    public bool feito {get; set;}
+}
--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using frontend.Data;
+using BlazorStrap;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddBlazorStrap();
 
 var app = builder.Build();
 
